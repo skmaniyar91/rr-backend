10ba493b13b5b1ad7769fe7edf3a88d584079507
samir <samirkhamaniyar@gmail.com>
2024-05-05 17:23:35 +0530

    login to logout

--- routes/auth.go
+++ routes/auth.go
@@ -11,6 +11,6 @@ func initAuthRoutes(apiGrp *echo.Group, config config.IAppConfig) {
 	authHandler := auth.NewAuthHandler(config)
 
 	apiGrp.GET("/login", authHandler.Login)
-	apiGrp.GET("/logout", authHandler.Login)
+	apiGrp.GET("/logout", authHandler.Logout)
 	apiGrp.GET("/login-test", authHandler.LoginTest, auth.Auth)
 }
